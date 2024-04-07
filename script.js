51d5bbd9d4e69509b4257f69eaefb811cf26e713
Bhargavi <144531363+Bhar02@users.noreply.github.com>
2024-04-07 17:30:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,69 @@
-// function fun()
-// {
-// var message= 'hello world';
-// var age=18;
-// age="done";
+function fun()
+{
+var message= 'hello world';
+var age=18;
+age="done";
 
-// console.log('hello');
-// console.log(message);
-// console.log(age);
-// for(var i=0;i<5;i++){
-// console.log('---',i);
-// }
-// console.log(i);
-// }
-// fun(); // fucntion calling
+console.log('hello');
+console.log(message);
+console.log(age);
+for(var i=0;i<5;i++){
+console.log('---',i);
+}
+console.log(i);
+}
+fun(); // fucntion calling
 
-// let message='fun';
-// console.log(message);
+let message='fun';
+console.log(message);
 
-// const id=45;
-// id='else';
-// console.log(id);
+const id=45;
+id='else';
+console.log(id);
 
 
-// let userAge=2;  //camel case
-// console.log(typeof(userAge)); 
-// age='else';
-// console.log(typeof(age));
-// let a=['a','b'];
-// a[0]='abc';
-// console.log(a);
+let userAge=2;  //camel case
+console.log(typeof(userAge)); 
+age='else';
+console.log(typeof(age));
+let a=['a','b'];
+a[0]='abc';
+console.log(a);
 
-// let str = '12';
-// let age = 20;
+let str = '12';
+let age = 20;
 
-// let ans = age-(-str); // addition doesn't work, so we use subtration
-// console.log(ans);  //string concatination
+let ans = age-(-str); // addition doesn't work, so we use subtration
+console.log(ans);  //string concatination
 
-//type checking
-//  let a='12';
-//  let b=12;
-// if(a==b)   //used for only value
-// {
-//     console.log("yes");
-// }
-// else{
-//     console.log("no");
-// }
+type checking
+ let a='12';
+ let b=12;
+if(a==b)   //used for only value
+{
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
 
-// if(a===b)   //used to check both the data type and value
-// {
-//     console.log("yes");
-// }
-// else{
-//     console.log("no");
-// }
-// let a = ["a", "b", "c", "d"];
-// let cpy = [...a];
-// console.log(cpy);
+if(a===b)   //used to check both the data type and value
+{
+    console.log("yes");
+}
+else{
+    console.log("no");
+}
+let a = ["a", "b", "c", "d"];
+let cpy = [...a];
+console.log(cpy);
 
 // !=  
 // !==
-// const str='bhargavi';
-// const ame='My name is: ';
-// const ans= `${ame} ${str}`;
-// console.log(ans);
+const str='bhargavi';
+const ame='My name is: ';
+const ans= `${ame} ${str}`;
+console.log(ans);
 
 var p = '1';
 var q = '2';
